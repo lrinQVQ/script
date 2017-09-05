1c1dc3982c2d5ac01ad8be6004b6fb9c559718fb
lrin <lrinQVQ@users.noreply.github.com>
2017-09-05 07:38:40 -0500

    Add files via upload

---
+++ feeds-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+./scripts/feeds update -a
+rm -rf feeds/applications/luci-app-shadowsocks-libev
+rm -rf feeds/packages/net/shadowsocks-client
+rm -rf feeds/packages/net/shadowsocks-libev
+./scripts/feeds update -a
+git clone https://github.com/lrinQVQ/openwrt-feeds.git package/feeds
+pushd package/feeds
+git submodule update --init --recursive
+popd
+./scripts/feeds install -a
