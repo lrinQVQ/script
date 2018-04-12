3e6fcc36eb4cd1effd3b941c31851aaa46e58cd0
lrin <lrinQVQ@users.noreply.github.com>
2018-04-12 08:10:08 +0800

    Update feeds-install.sh

--- openwrt/feeds-install.sh
+++ openwrt/feeds-install.sh
@@ -3,6 +3,7 @@
 rm -rf feeds/luci/applications/luci-app-shadowsocks-libev
 rm -rf feeds/packages/net/shadowsocks-client
 rm -rf feeds/packages/net/shadowsocks-libev
+rm -rf package/libs/mbedtls
 ./scripts/feeds update -a
 git clone https://github.com/lrinQVQ/openwrt-feeds.git package/feeds
 pushd package/feeds
